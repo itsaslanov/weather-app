82981904fd906c60439f362ad1c58ba53e50349c
Azizbek Aslanov <its.aslanov@gmail.com>
2022-07-19 10:59:47 +0500

    script fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'clear-weather': "url('/src/assets/clear.jpg')",
-        'sunny-weather': "url('/src/assets/sunny.jpg')",
-        'cloudy-weather': "url('/src/assets/cloudy.jpg')",
-        'rain-weather': "url('/src/assets/rain.jpg')",
-        'snow-weather': "url('/src/assets/snow.jpg')",
-        'sky': "url('/src/assets/sky.jpg')",
+        'clear-weather': "url('../src/assets/clear.jpg')",
+        'sunny-weather': "url('../src/assets/sunny.jpg')",
+        'cloudy-weather': "url('../src/assets/cloudy.jpg')",
+        'rain-weather': "url('../src/assets/rain.jpg')",
+        'snow-weather': "url('../src/assets/snow.jpg')",
+        'sky': "url('../src/assets/sky.jpg')",
       }
     },
   },
